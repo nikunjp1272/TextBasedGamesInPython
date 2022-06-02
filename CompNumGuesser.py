a8dbb035e20f862c00069783fdb6edb934a0bdcf
nikunjp1272 <86245332+nikunjp1272@users.noreply.github.com>
2022-06-03 02:36:09 +0530

    added another version
    another version of computer number guesser

--- CompNumGuesser.py
+++ CompNumGuesser.py
@@ -16,3 +16,28 @@ def comp_guess(x):
   print(f'Your guessed number is {guess}')
 
 comp_guess(10000)
+
+#Another approach
+#Uncomment the code below by removing the --> ''' symbols at the beginning and end
+'''
+#Importing the random library for selecting a random number
+import random
+
+#defining a function by the name of guess
+
+def guess(x):
+#choosing a number randomly from 1 to x
+  random_n=random.randint(1,x)
+  guess=0
+#run a while loop to check if the number is equal to the guess, if not return low or high acoordingly
+  while guess!=random_n:
+    guess=int(input(f'Guess the n between 1 and {x}: '))
+    print(guess)
+    if guess < random_n:
+       print('Too low')
+    elif guess > random_n:
+      print('Too high')
+#print this after the number=guess
+  print('You guessed the number correctly')
+guess(100)
+'''
